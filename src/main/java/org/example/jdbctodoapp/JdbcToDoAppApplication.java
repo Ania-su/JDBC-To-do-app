4a9d2b11e062aaaa913fe27b350cb75c7130f27b
Ania-su <antsaaa.randria@gmail.com>
2025-08-28 11:08:49 +0300

    project initialization

---
+++ src/main/java/org/example/jdbctodoapp/JdbcToDoAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.jdbctodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcToDoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcToDoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jdbctodoapp/JdbcToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jdbctodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcToDoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

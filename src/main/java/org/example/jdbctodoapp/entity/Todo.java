9cf7f0e61621aae69d8850e85c7d9109c4cb78af
Ilo <hei.iloniaina@gmail.com>
2025-08-29 08:56:14 +0300

    feat: todo entity

---
+++ src/main/java/org/example/jdbctodoapp/entity/Todo.java
@@ -0,0 +1,17 @@
+package org.example.jdbctodoapp.entity;
+
+import lombok.*;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Todo {
+    private int id;
+    private String title;
+    private Boolean isCompleted;
+    private Instant createdAt;
+}

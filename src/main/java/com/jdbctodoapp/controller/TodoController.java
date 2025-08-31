f60ff6c1e0b6eb2ac63459ca57ec071ebd44b0f1
Ania-su <antsaaa.randria@gmail.com>
2025-08-31 14:32:28 +0300

    feat: add TodoController and TodoService

--- src/main/java/com/jdbctodoapp/controller/TodoController.java
+++ src/main/java/com/jdbctodoapp/controller/TodoController.java
@@ -1,4 +1,47 @@
 package com.jdbctodoapp.controller;
 
+import com.jdbctodoapp.entity.Todo;
+import com.jdbctodoapp.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController {
+
+    private final TodoService service;
+
+    public TodoController(TodoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Todo> getAll(@RequestParam(value = "isCompleted", required = false) Boolean isCompleted) {
+        return service.getAll(isCompleted);
+    }
+
+    @GetMapping("/{id}")
+    public Todo getById(@PathVariable int id) {
+        return service.getById(id).orElseThrow(() -> new RuntimeException("Todo not found"));
+    }
+
+    @PostMapping
+    public String create(@RequestBody Todo todo) {
+        service.addTodo(todo);
+        return "Todo created";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable int id, @RequestBody Todo todo) {
+        todo.setId(id);
+        service.update(todo);
+        return "Todo updated";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        service.delete(id);
+        return "Todo deleted";
+    }
 }
--- src/main/java/com/jdbctodoapp/service/TodoService.java
+++ src/main/java/com/jdbctodoapp/service/TodoService.java
@@ -1,4 +1,38 @@
 package com.jdbctodoapp.service;
 
+import com.jdbctodoapp.entity.Todo;
+import com.jdbctodoapp.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TodoService {
+
+    private final TodoRepository repository;
+
+    public TodoService(TodoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Todo> getAll(Boolean isCompleted) {
+        return repository.findAll(isCompleted);
+    }
+
+    public Optional<Todo> getById(int id) {
+        return repository.findById(id);
+    }
+
+    public int addTodo(Todo todo) {
+        return repository.save(todo);
+    }
+
+    public int update(Todo todo) {
+        return repository.update(todo);
+    }
+
+    public int delete(Long id) {
+        return repository.deleteById(id);
+    }
 }
